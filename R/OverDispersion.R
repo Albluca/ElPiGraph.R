ae2e154b235dc54cc1f3b9326eced8c9bbecd6a1
Luca Albergante <lucaalbergante@Lucas-MacBook-Pro.local>
2018-01-10 17:22:38 +0100

    commented functions related to overdispersion / moved pseudotime functions to a different file

--- R/OverDispersion.R
+++ R/OverDispersion.R
@@ -1,83 +1,197 @@
-#' Title
-#'
-#' @param X 
-#' @param NumNodes 
-#' @param Subset 
-#' @param Samples 
-#' @param Type 
-#' @param Exclusive 
-#' @param ... 
-#'
-#' @return
-#' @export
-#'
-#' @examples
-StudyOverDispersion <- function(X, NumNodes, Subset, Samples = 100, Type = "Curve", Exclusive = FALSE, ...) {
-  
-  # X = t(SelData)
-  # NumNodes = 30
-  # Samples = 100
-  # Exclusive = FALSE
-  # Subset <- sample(colnames(X), 30)
-  
-  Subset <- Subset[Subset %in% colnames(X)]
-  
-  if(Exclusive){
-    Selectionable <- setdiff(colnames(X), Subset)
-  } else {
-    Selectionable <- colnames(X)
-  }
-  
-  Subsets <- lapply(1:Samples, function(i){
-    sample(Selectionable, length(Subset))
-  })
-  Subsets <- append(list(Subset), Subsets)
-  
-  if(Type == "Curve"){
-    
-    tictoc::tic()
-    Base <- computeElasticPrincipalCurve(X = X, NumNodes = NumNodes, drawAccuracyComplexity = FALSE,
-                                        drawEnergy = FALSE, drawPCAView = FALSE, Subsets = Subsets, ...)
-    tictoc::toc()
-    
-  }
-  
-  
-  if(Type == "Tree"){
-    
-    tictoc::tic()
-    Base <- computeElasticPrincipalTree(X = X, NumNodes = NumNodes, drawAccuracyComplexity = FALSE,
-                                        drawEnergy = FALSE, drawPCAView = FALSE, Subsets = Subsets, ...)
-    tictoc::toc()
-    
-  }
-  
-  
-  if(Type == "Circle"){
-    
-    tictoc::tic()
-    Base <- computeElasticPrincipalCircle(X = X, NumNodes = NumNodes, drawAccuracyComplexity = FALSE,
-                                        drawEnergy = FALSE, drawPCAView = FALSE, Subsets = Subsets, ...)
-    tictoc::toc()
-    
-  }
-  
-  ReportData <- sapply(Base, "[[", "FinalReport")
-  FVE.Sampled <- unlist(ReportData["FVE",])
-  FVEP.Sampled <- unlist(ReportData["FVEP",])
-  
-  par(mfcol = c(1,2))
-  
-  boxplot(FVE.Sampled[-1], at = 1, main = "FVE")
-  points(x = 1, FVE.Sampled[1], col="red", pch = 20)
-  
-  boxplot(FVEP.Sampled[-1], at = 1, main = "FVEP")
-  points(x = 1, FVEP.Sampled[1], col="red", pch = 20)
-  
-  par(mfcol = c(1,1))
-     
-  return(list(Data = Base,
-           FVE = wilcox.test(FVE.Sampled[-1] - FVE.Sampled[1], alternative = "less")$p.value,
-           FVEP = wilcox.test(FVEP.Sampled[-1] - FVEP.Sampled[1], alternative = "less")$p.value))
-  
-}
+# StudyOverDispersion <- function(X, NumNodes, Subset, Samples = 100, Type = "Curve", Exclusive = FALSE, ...) {
+#   
+#   # X = t(SelData)
+#   # NumNodes = 30
+#   # Samples = 100
+#   # Exclusive = FALSE
+#   # Subset <- sample(colnames(X), 30)
+#   
+#   Subset <- Subset[Subset %in% colnames(X)]
+#   
+#   if(Exclusive){
+#     Selectionable <- setdiff(colnames(X), Subset)
+#   } else {
+#     Selectionable <- colnames(X)
+#   }
+#   
+#   Subsets <- lapply(1:Samples, function(i){
+#     sample(Selectionable, length(Subset))
+#   })
+#   Subsets <- append(list(Subset), Subsets)
+#   
+#   if(Type == "Curve"){
+#     
+#     tictoc::tic()
+#     Base <- computeElasticPrincipalCurve(X = X, NumNodes = NumNodes, drawAccuracyComplexity = FALSE,
+#                                         drawEnergy = FALSE, drawPCAView = FALSE, Subsets = Subsets, ...)
+#     tictoc::toc()
+#     
+#   }
+#   
+#   
+#   if(Type == "Tree"){
+#     
+#     tictoc::tic()
+#     Base <- computeElasticPrincipalTree(X = X, NumNodes = NumNodes, drawAccuracyComplexity = FALSE,
+#                                         drawEnergy = FALSE, drawPCAView = FALSE, Subsets = Subsets, ...)
+#     tictoc::toc()
+#     
+#   }
+#   
+#   
+#   if(Type == "Circle"){
+#     
+#     tictoc::tic()
+#     Base <- computeElasticPrincipalCircle(X = X, NumNodes = NumNodes, drawAccuracyComplexity = FALSE,
+#                                         drawEnergy = FALSE, drawPCAView = FALSE, Subsets = Subsets, ...)
+#     tictoc::toc()
+#     
+#   }
+#   
+#   ReportData <- sapply(Base, "[[", "FinalReport")
+#   FVE.Sampled <- unlist(ReportData["FVE",])
+#   FVEP.Sampled <- unlist(ReportData["FVEP",])
+#   
+#   par(mfcol = c(1,2))
+#   
+#   boxplot(FVE.Sampled[-1], at = 1, main = "FVE")
+#   points(x = 1, FVE.Sampled[1], col="red", pch = 20)
+#   
+#   boxplot(FVEP.Sampled[-1], at = 1, main = "FVEP")
+#   points(x = 1, FVEP.Sampled[1], col="red", pch = 20)
+#   
+#   par(mfcol = c(1,1))
+#      
+#   return(list(Data = Base,
+#            FVE = wilcox.test(FVE.Sampled[-1] - FVE.Sampled[1], alternative = "less")$p.value,
+#            FVEP = wilcox.test(FVEP.Sampled[-1] - FVEP.Sampled[1], alternative = "less")$p.value))
+#   
+# }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 
+# ExploreVarianceOverCurve <- function(X, NumNodes, Type = "Curve",
+#                                              nReps = 100, ProbPoint = .9, ...) {
+#   
+#   if(Type == "Curve"){
+#     TargetStruct <- computeElasticPrincipalCurve(X = X, NumNodes = NumNodes,
+#                                                  nReps = nReps, ProbPoint = ProbPoint,
+#                                                  ...)
+#   }
+#   
+#   if(Type == "Tree"){
+#     TargetStruct <- computeElasticPrincipalTree(X = X, NumNodes = NumNodes,
+#                                                  nReps = nReps, ProbPoint = ProbPoint,
+#                                                  ...)
+#   }
+#   
+#   if(Type == "Circle"){
+#     TargetStruct <- computeElasticPrincipalCircle(X = X, NumNodes = NumNodes,
+#                                                 nReps = nReps, ProbPoint = ProbPoint,
+#                                                 ...)
+#   }
+#   
+#   
+#   
+#   PointProj <- lapply(TargetStruct[1:nReps], function(TS){
+#      project_point_onto_graph(X = X, NodePositions = TS$NodePositions,
+#                                           Edges = TS$Edges$Edges, Partition = NULL)
+#   })
+#   
+#   FVE <- sapply(TargetStruct[1:nReps], function(TS){
+#     c(TS$FinalReport$FVEP, TS$FinalReport$FVE)
+#   })
+#   rownames(FVE) <- c("FVEP", "FVE")
+#   
+#   
+#   
+#   PCAData <- irlba::prcomp_irlba(x = X, n = 1, retx = FALSE)
+#   PC1_FVE <- (PCAData$sdev^2)/sum(apply(X, 2, var))
+#   
+#   
+#   
+#   p <- ggplot2::ggplot(data = reshape::melt(data.frame(t(FVE))),
+#                   mapping = ggplot2::aes(x = variable, y = value, fill = variable)) +
+#     ggplot2::geom_boxplot() +
+#     ggplot2::labs(y = "Fraction of explained variance", x = "Calculation mode") +
+#     ggplot2::guides(fill = "none")
+#     
+#   p1 <- ggplot2::ggplot(data = reshape::melt(data.frame(t(FVE))),
+#                        mapping = ggplot2::aes(x = variable, y = value/PC1_FVE, fill = variable)) +
+#     # ggplot2::scale_y_log10() +
+#     ggplot2::geom_boxplot() +
+#     ggplot2::labs(y = "Ratio of fractions of explained variance (ElPiGr/PC1)", x = "Calculation mode")  +
+#     ggplot2::guides(fill = "none")
+#     
+#   ggpubr::ggarrange(p, p1, ncol=2)
+#   
+#   if(nReps > 1){
+#     PointProj <- project_point_onto_graph(X = X,
+#                              NodePositions = TargetStruct[[nReps+1]]$NodePositions,
+#                              Edges = TargetStruct[[nReps+1]]$Edges$Edges,
+#                              Partition = NULL)
+#   } else {
+#     PointProj <- project_point_onto_graph(X = X,
+#                              NodePositions = TargetStruct[[1]]$NodePositions,
+#                              Edges = TargetStruct[[1]]$Edges$Edges,
+#                              Partition = NULL)
+#   }
+#   
+#   
+#   
+#   
+#   
+#   # 
+#   # PointProj$
+#   # 
+#   # 
+#   # 
+#   # X_Corrected <- X - PointProj$X_projected
+#   # Distances <- rowSums(X_Corrected^2)
+#   # 
+#   # 
+#   # 
+#   # var(PointProj$X_projected)/ sum(apply(X_Corrected, 2, var))
+#   # 
+#   # boxplot(Distances)
+#   # boxplot(Distances ~ PointProj$EdgeID, las = 2)
+#   
+#   
+#   
+# }
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# # 
+# # 
+# # 
+# # ElPiROMA <- function(X) {
+# #   
+# # }
+# # 
---
+++ R/pseudotime.R
@@ -0,0 +1,232 @@
+#' Obtain the pseudotime associted with a path
+#'
+#' @param ProjStruct
+#' @param EdgeSeq
+#'
+#' @return
+#' @export
+#'
+#' @examples
+getPseudotime <- function(ProjStruct, NodeSeq){
+  
+  Pt <- rep(NA, nrow(ProjStruct$X_projected))
+  tLen <- 0
+  NodePos <- 0
+  
+  for(i in 2:length(NodeSeq)){
+    SelEdgID <- which(
+      apply(
+        apply(ProjStruct$Edges, 1, function(x) {
+          x %in% NodeSeq[(i-1):i]
+        }), 2, all)
+    )
+    
+    # print(paste(i, SelEdgID, tLen))
+    
+    if(length(SelEdgID) != 1){
+      stop("Path not found in the graph")
+    }
+    
+    Selected <- ProjStruct$EdgeID == SelEdgID
+    if(all(ProjStruct$Edges[SelEdgID,] == NodeSeq[(i-1):i])){
+      rev <- FALSE
+    } else {
+      rev <- TRUE
+    }
+    
+    Pos <- ProjStruct$ProjectionValues[Selected]
+    Pos[Pos <= 0] <- 0
+    Pos[Pos >= 1] <- 1
+    
+    if(rev){
+      Pos <- 1 - Pos
+    }
+    
+    if(sum(Selected)>0){
+      Pt[Selected] <- tLen + Pos*ProjStruct$EdgeLen[SelEdgID]
+    }
+    
+    tLen <- tLen + ProjStruct$EdgeLen[SelEdgID]
+    NodePos <- cbind(NodePos, tLen)
+  }
+  
+  return(list(Pt = Pt, PathLen = tLen, NodePos = as.vector(NodePos)))
+  
+}
+
+
+
+
+
+
+
+
+
+#' Title
+#'
+#' @param ExpData 
+#' @param Paths 
+#' @param TargetPG 
+#' @param Partition 
+#' @param PrjStr 
+#' @param Main 
+#' @param genes 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+CompareExpOnBranches <- function(ExpData,
+                                 Paths,
+                                 TargetPG,
+                                 Partition,
+                                 PrjStr,
+                                 Main = "",
+                                 genes = 4) {
+  
+  CombDF <- NULL
+  
+  if(is.numeric(genes)){
+    
+    GenesByVar <- apply(ExpData, 1, var)
+    
+    if(length(GenesByVar) <= genes){
+      genes = length(GenesByVar)
+    }
+    
+    genes <- names(GenesByVar[order(GenesByVar, decreasing = TRUE)])[1:genes]
+    
+  }
+  
+  tExpData <- ExpData[genes, ]
+  
+  for(i in 1:length(Paths)){
+    PtOnPath <- getPseudotime(ProjStruct = PrjStr, NodeSeq = Paths[[i]])
+    
+    PtVect <- PtOnPath$Pt
+    names(PtVect) <- colnames(tExpData)
+    MetlExp <- reshape::melt(t(tExpData))
+    
+    CombDF <- rbind(
+      CombDF, data.frame(Pt = PtVect[as.character(MetlExp$X1)],
+                         gene = MetlExp$X2, exp = MetlExp$value,
+                         traj = i)
+    )
+    
+  }
+  
+  CombDF$traj <- factor(CombDF$traj)
+  
+  CombDF <- CombDF[!is.na(CombDF$Pt), ]
+  
+  if(nrow(CombDF)>0){
+    p <- ggplot2::ggplot(CombDF,
+                         ggplot2::aes(x=Pt, y=exp, color = traj)) + ggplot2::geom_point(alpha = .3) +
+      ggplot2::geom_smooth() + ggplot2::scale_color_discrete("Branch") +
+      ggplot2::facet_wrap(~gene, scales = "free_y") +
+      ggplot2::labs(title = Main, y = "Gene expression", x = "Pseudotime")
+    
+    return(p)
+    
+  } else {
+    
+    return(NULL)
+    
+  }
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#' Title
+#'
+#' @param ExpData 
+#' @param Paths 
+#' @param TargetPG 
+#' @param Partition 
+#' @param PrjStr 
+#' @param Main 
+#' @param genes 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+ExpOnPath <- function(ExpData,
+                      Group = NULL,
+                      Path,
+                      TargetPG,
+                      Partition,
+                      PrjStr,
+                      Main = "",
+                      genes = 4) {
+  
+  
+  if(is.null(Group)){
+    Group <- rep("N/A", ncol(ExpData))
+  }
+  
+  if(is.numeric(genes)){
+    
+    GenesByVar <- apply(ExpData, 1, var)
+    
+    if(length(GenesByVar) <= genes){
+      genes = length(GenesByVar)
+    }
+    
+    genes <- names(GenesByVar[order(GenesByVar, decreasing = TRUE)])[1:genes]
+    
+  }
+  
+  tExpData <- ExpData[genes, ]
+  
+  PtOnPath <- getPseudotime(ProjStruct = PrjStr, NodeSeq = Path)
+  
+  PtVect <- PtOnPath$Pt
+  names(PtVect) <- colnames(tExpData)
+  MetlExp <- reshape::melt(t(tExpData))
+  
+  CombDF <- data.frame(Pt = PtVect[as.character(MetlExp$X1)],
+                       gene = MetlExp$X2, exp = MetlExp$value,
+                       group = Group)
+  
+  CombDF <- CombDF[!is.na(CombDF$Pt), ]
+  
+  if(nrow(CombDF)>0){
+    p <- ggplot2::ggplot(CombDF,
+                         ggplot2::aes(x=Pt, y=exp)) +
+      ggplot2::geom_point(ggplot2::aes(color = group), alpha = .3) +
+      ggplot2::geom_smooth() + ggplot2::scale_color_discrete("Group") +
+      ggplot2::facet_wrap(~gene, scales = "free_y") +
+      ggplot2::labs(title = Main, y = "Gene expression", x = "Pseudotime")
+    
+    return(p)
+    
+  } else {
+    
+    return(NULL)
+    
+  }
+  
+}
+
+
